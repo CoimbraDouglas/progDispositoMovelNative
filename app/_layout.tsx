5ffbcd18630f834e4fc751405206e9129139ccef
DouglasCoimbra <coimbra.doglas@hotmail.com>
2025-09-24 21:46:09 -0300

    Projeto da aula, criar menu interativo react-native-paper

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+
+import { Slot } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import {PaperProvider} from 'react-native-paper';
+export default function Layout() {
+  return(
+    <PaperProvider>
+      <View style= {styles.container}>
+        <Slot />
+      </View>  
+    </PaperProvider>
+  );
+}
+
+// versão atualizada
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff'
+  }
+});
